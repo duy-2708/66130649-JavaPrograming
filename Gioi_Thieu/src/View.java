4fca24fb96154598666901954bec1ffca0e725fd
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-01-29 11:04:37 +0700

    Vi du 1
    View 1

---
+++ Gioi_Thieu/src/View.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class View {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Đặng Ngọc Nhật Duy ");
+		System.out.println("66130649");
+	}
+
+}
