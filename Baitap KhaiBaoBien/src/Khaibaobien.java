8776c735fb819dd9b09bcfed4bc058f93a5d8800
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-02-04 20:28:58 +0700

    Hoàn thiện bài 1
    Khai báo biến

---
+++ Baitap KhaiBaoBien/src/Khaibaobien.java
@@ -0,0 +1,17 @@
+
+public class Khaibaobien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 10 ;
+		double b = 15 ; 
+		double c = 3.5 ;
+		
+		double tong = a+b ;
+		double thuong  = 1.0*(a / b );
+		
+		System.out.println("Tong  " + tong);
+		System.out.print("Thuong la : " + thuong + "\n");
+	}
+
+}
