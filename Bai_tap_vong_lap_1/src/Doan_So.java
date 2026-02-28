4f777e6ac28882557ebaa63839c5e2e492e987f5
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-02-28 18:54:56 +0700

    Doan_So
    5.1

---
+++ Bai_tap_vong_lap_1/src/Doan_So.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Doan_So {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		do {
+			
+			int m = 10 ;
+			int random = nextINT(100);
+			for(int i = 0 ; i<= 10 ; i++) {
+				if(i != radom ) {
+					m--;
+				}else {
+					break ;
+				}
+			}
+			if(m == 0 ) {
+				System.out.println("Ban da het luot ");
+			}
+			if(100- )
+		}while(i != random);
+	}
+
+}
