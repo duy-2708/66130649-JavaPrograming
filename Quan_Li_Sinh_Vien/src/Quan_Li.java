b0bcd71b42cee23e312eba2a257a9de3dffd36ff
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-03-03 20:30:16 +0700

    Update Quan_Li

--- Quan_Li_Sinh_Vien/src/Quan_Li.java
+++ Quan_Li_Sinh_Vien/src/Quan_Li.java
@@ -5,23 +5,29 @@ public class Quan_Li {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap so luong sinh vien ");
 		 int n = sc.nextInt();
-		 int a [] = new int [n];
-		nhap(sc,a,n);
-		xuat(a,n);
+		 String [] tenSinhVien = new String[n];
+		double [] diemSinhVien = new double[n];
+		nhap(sc,tenSinhVien,diemSinhVien,n);
+		xuat(tenSinhVien,diemSinhVien,n);
 		
 	}
-	private static void nhap(Scanner sc,int a[],int n) {
+	private static void nhap(Scanner sc,String[] tenSinhVien ,double diemSinhVien [] ,int n) {
 		System.out.println("Nhap so luong hoc sinh ten va diem ");
 		for(int i = 0 ; i < n ; i++) {
-			a[i] = sc.nextInt();
-			String [] tenSinhVien = new String[n];
-			double [] diemSinhVien = new double[n];
+			System.out.println("Nhap so sinh vien thu "+(i+1)+"---");
+			sc.nextLine();
+			System.out.println("Nhap ten ");
+			tenSinhVien [i] = sc.nextLine();
+			System.out.println("Nhap diem ");
+			diemSinhVien [i] = sc.nextDouble();
 		}
 	}
-	private static void xuat(int a[] , int n) {
+	private static void xuat(String tenSinhVien[], double diemSinhVien[],int n) {
+		System.out.println("Danh Sach Sinh Vien ");
 		for(int i = 0 ; i < n ; i++ ) {
-			System.out.printf("%d ",a[i]);
+			System.out.printf("Ten: %s , Diem: %.2f\n ",tenSinhVien[i],diemSinhVien[i]);
 		}
 	}
 }
