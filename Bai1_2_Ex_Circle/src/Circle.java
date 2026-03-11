6da73c82547ce8836d78ff65924b07ec4d514ee6
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-03-11 20:21:05 +0700

    Ex_Circle_1_2

---
+++ Bai1_2_Ex_Circle/src/Circle.java
@@ -0,0 +1,30 @@
+public class Circle {
+		   private double radius;
+		   public Circle() {
+			   radius = 1.0;
+		   }
+		   public Circle(double r) {
+			   radius = r;
+		   }
+//		   public Circle(double radius) {
+//			   this.radius = radius;
+//			   color = "red";
+//		   }
+		   //getter
+		   public double getRadius() {
+			   return radius;
+		   }
+		   public double getArea() {
+			   return radius * radius * Math.PI;
+		   }
+		   public double getCircumference() {
+			   return 2 * Math.PI * radius;
+		   }
+		   //setter
+		   public void setRadius(double newRadius) {
+			   radius = newRadius;
+		   } 
+		   public String toString() {
+			   return "Circle[Bán kính = " + radius + "]";
+		   }
+		}
