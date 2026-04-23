a4cba3770523c9eeb4648ee4a9d269715bbc23ae
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-04-23 07:36:26 +0700

    Cau 2
    Tao class San Pham

---
+++ ThiGk2_DangNgocNhatDuy/src/thigk2/dangngocnhaduy/SanPham.java
@@ -0,0 +1,38 @@
+package thigk2.dangngocnhaduy;
+
+public class SanPham {
+	private String maSP ;
+	private String tenSP ;
+	private String loaiSP ;
+	private double giaSP ;
+	public String getMaSP() {
+		return maSP;
+	}
+	public void setMaSP(String maSP) {
+		this.maSP = maSP;
+	}
+	public String getTenSP() {
+		return tenSP;
+	}
+	public void setTenSP(String tenSP) {
+		this.tenSP = tenSP;
+	}
+	public String getLoaiSP() {
+		return loaiSP;
+	}
+	public void setLoaiSP(String loaiSP) {
+		this.loaiSP = loaiSP;
+	}
+	public double getGiaSP() {
+		return giaSP;
+	}
+	public void setGiaSP(double giaSP) {
+		this.giaSP = giaSP;
+	}
+	public void hienThiThongTin() {
+		System.out.println("Ma SP: "+maSP);
+		System.out.println("Ten SP: "+tenSP );
+		System.out.println("Loai SP: "+loaiSP );
+		System.out.println("Gia SP: "+giaSP);
+	}
+}
