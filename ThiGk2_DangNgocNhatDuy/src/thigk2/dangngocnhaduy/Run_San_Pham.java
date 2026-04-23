b512e652141e94052a3b1984f09ea27faf563414
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-04-23 07:56:30 +0700

    Thuc Pham Chuc Nang
    In ra danh sach laoi thuc pham

--- ThiGk2_DangNgocNhatDuy/src/thigk2/dangngocnhaduy/Run_San_Pham.java
+++ ThiGk2_DangNgocNhatDuy/src/thigk2/dangngocnhaduy/Run_San_Pham.java
@@ -6,7 +6,8 @@ public class Run_San_Pham {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ArrayList<SanPham> ds = new ArrayList();
-		int n = 3 ;
+		ArrayList<SanPham> ds2 = new ArrayList();
+		int n = 1 ;
 		for( int i = 0 ; i< n ; i++) {
 			SanPham spi = new SanPham();
 			System.out.println("Nhap maSP: ");
@@ -22,6 +23,30 @@ public class Run_San_Pham {
 		}
 		for(SanPham sp: ds ) {
 			sp.hienThiThongTin();
+		}
+		SanPham spt = new SanPham();
+		System.out.println("Them moi 1 san pham: ");
+		System.out.println("Nhap maSP: ");
+		spt.setMaSP(sc.nextLine());
+		System.out.println("Nhap tenSP: ");
+		spt.setTenSP(sc.nextLine());
+		System.out.println("Nhap loaiSP: ");
+		spt.setLoaiSP(sc.nextLine());
+		System.out.println("Nhap giaSP: ");
+		spt.setGiaSP(sc.nextDouble());
+		sc.nextLine();
+		ds.add(spt);
+		for(SanPham sp: ds ) {
+			sp.hienThiThongTin();
+		}
+		System.out.println("Danh sach san pham loai Thuc Pham Chuc Nang: ");
+		for(int i = 0 ; i< ds.size(); i++) {
+			if(ds.get(i).getLoaiSP().equals("Thuc Pham Chuc Nang ")) {
+				ds2.add(ds.get(i));
+			}
+		}
+		for(SanPham sp2 :ds2 ) {
+			sp2.hienThiThongTin();
 		}
 	}
 
