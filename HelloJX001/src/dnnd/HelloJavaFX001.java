b9a75fd0d1a757ee4c669d965dfb768aa66145f0
duy-2708 <duy.dnn.66cntt@ntu.edu.vn>
2026-05-07 19:06:16 +0700

    HelloJavaFX001

---
+++ HelloJX001/src/dnnd/HelloJavaFX001.java
@@ -0,0 +1,31 @@
+package dnnd;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class HelloJavaFX001 extends Application{
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Button btn = new Button();
+		btn.setText("Xin chao");
+		StackPane root = new StackPane();
+		root.getChildren().add(btn);
+		Scene scn = new Scene(root,400,200);
+		primaryStage.setTitle("Hello Word ");
+		primaryStage.setScene(scn);
+		primaryStage.show();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+	
+
+}
